326e7ddb2f04db226f6b44dfdc5e3634245e3e7f
Yoann Deblois <yoann.deblois@gmail.com>
2021-06-15 07:27:58 +0200

    Keeping the Dice instead of Dumping them + optimisation of the user path

--- 421.py
+++ 421.py
@@ -1,30 +1,47 @@
 import random
 
-reponse = ""
-lancers =[]
+reponse=""
+	
+def check_fin_tour(choix_joueur_1):
+		if choix_joueur_1.find("1")!=(-1) and choix_joueur_1.find("2")!=(-1) and choix_joueur_1.find("3")!=(-1):
+			continuer="non"
+		else:
+			continuer="oui"
+		return continuer
 
-def relancer(lancer):
-	choix_joueur=str(input("voulez-vous relancer les dés 1, 2 et/ou 3?"))
-	if choix_joueur.find("1")!=(-1):
+def change_dices(lancer, choix_joueur_1):
+	if choix_joueur_1.find("1")==(-1):
 		lancer[0]=random.randint(1,6)
-	if choix_joueur.find("2")!=(-1):
+	if choix_joueur_1.find("2")==(-1):
 		lancer[1]=random.randint(1,6)
-	if choix_joueur.find("3")!=(-1):
+	if choix_joueur_1.find("3")==(-1):
 		lancer[2]=random.randint(1,6)
 	return lancer
 
 
 print("Le 421")
-lancers=[random.randint(1,6), random.randint(1,6),random.randint(1,6)]
-print ("Lancer 1",lancers)
-reponse_lancer=input("Voulez-vous relancer les dés ? O=oui, N=non")
-if reponse_lancer=="O":
-  relancer(lancers)
-  print("Lancer 2",lancers)
-  reponse_lancer=input("Voulez-vous relancer les dés ? O=oui, N=non")
-  if reponse_lancer=="O":
-    relancer(lancers)
-    print("Lancer 3",lancers)
-  
-print("résultat final :",lancers)
-print("Au revoir")
+while reponse!="Q":
+	lancers=[random.randint(1,6), random.randint(1,6),random.randint(1,6)]
+	print ("Lancer 1",lancers)
+	nb_lancers=1
+	choix_joueur=str(input("Voulez-vous garder les dés 1, 2 et/ou 3 ?"))
+	continuer=check_fin_tour(choix_joueur)
+	if continuer=="oui":
+		lancers=change_dices(lancers, choix_joueur)
+		print("Lancer 2",lancers)
+		nb_lancers+=1
+		choix_joueur=str(input("Voulez-vous garder les dés 1, 2 et/ou 3 ?"))
+		continuer=check_fin_tour(choix_joueur)
+		if continuer=="oui":
+	   		lancers=change_dices(lancers, choix_joueur)
+	   		print("Lancer 3",lancers)
+	   		nb_lancers+=1
+	if nb_lancers == 1:
+		print("Résultat final :",lancers,"en", nb_lancers,"lancer.")
+	else:
+		print("Résultat final :",lancers,"en", nb_lancers,"lancers.")
+	reponse=input("Entrée pour rejouer, Q pour quitter")
+	reponse=reponse.upper()
+	print("")
+
+print("Au revoir !")
